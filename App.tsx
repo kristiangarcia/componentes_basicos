2096d32e9fb222e20f71501da2c09117b3113b44
kristiangarcia <kristiangarciapaulsen@gmail.com>
2025-09-30 21:17:28 +0200

    Creado el contenedor principal

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.contenedorPrincipal}>
+      <Text>App</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  contenedorPrincipal: {
+    padding: 20,
+    backgroundColor: '#f5f3f3ff',
+    width: '100%',
+    height: '100%'
+  }
+})
