fc01bdc5ab5fe5b47a22b0915e4bb649f6b0f974
kristiangarcia <kristiangarciapaulsen@gmail.com>
2025-09-30 21:28:19 +0200

    Logo encerrado en zona segura

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Image } from 'expo-image'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function App() {
   return (
+    <SafeAreaView edges={['top', 'bottom']}>
     <View style={styles.contenedorPrincipal}>
       <Image
         source={require("./assets/registrar.jpg")}
@@ -11,12 +13,12 @@ export default function App() {
         style={styles.logo}
         />
     </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   contenedorPrincipal: {
-    padding: 20,
     backgroundColor: '#f5f3f3ff',
     width: '100%',
     height: '100%'
